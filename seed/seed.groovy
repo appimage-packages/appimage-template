da9398ec742df0f53064cb1e05ca6b418cacf313
Scarlett Clark <sgclark@kde.org>
2016-12-06 13:05:25 -0700

    try WORKSACE

--- seed/seed.groovy
+++ seed/seed.groovy
@@ -8,7 +8,7 @@ import java.net.URL;
 import java.security.Provider.Service
 import com.google.common.io.ByteStreams
 
-def apps = new Yaml().load(new FileReader(new File('applications.yaml')))
+def apps = new Yaml().load(new FileReader(new File("${WORKSPACE}/seed/applications.yaml")))
 
 apps.each { name, config ->
   multibranchPipelineJob("pipeline-${name}-appimage") {
