caa6242a3d59f76e350d9a1bf94b260e1c6d78b4
Scarlett Clark <sgclark@kde.org>
2017-01-31 15:31:37 -0700

    Add github properties

--- seed/seed.groovy
+++ seed/seed.groovy
@@ -14,6 +14,9 @@ apps.each { name, config ->
   config.branch.each { branch ->
     pipelineJob("${name}-${branch}-appimage") {
      logRotator(daysToKeep = -1, numToKeep = 5, artifactDaysToKeep = -1, artifactNumToKeep = -1)
+     properties {
+        githubProjectUrl"https://github.comappimage-packages/${name}", "${branch}")
+    }
      definition {
         cpsScm {
             scm {
