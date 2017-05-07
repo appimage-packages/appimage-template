2869e76e66c6cc9b46ba0554f7b90b6dc7cb1fc8
Scarlett Clark <sgclark@kde.org>
2017-05-07 06:05:22 -0700

    Fix template

--- docker/qt59/qt5.sh
+++ docker/qt59/qt5.sh
@@ -7,5 +7,5 @@ cd /source && git clone git://code.qt.io/qt/qt5.git && cd qt5 && git checkout 5.
 git fetch https://codereview.qt-project.org/qt/qt5 refs/changes/44/189744/2 && git format-patch -1 --stdout FETCH_HEAD > init.patch
 patch -p1 < init.patch
 perl init-repository --module-subset=default,-qtmacextras,-qtwinextras,-qtwebkit-examples,-qtandroidextras
-cd /source/qt5 && ./configure -v -developer-build -opensource -nomake examples -nomake tests -confirm-license -prefix /opt/usr && make  -j8 && make install && rm -rfv $SOURCES/qt5
+cd /source/qt5 && ./configure -v -developer-build -opensource -nomake examples -nomake tests -confirm-license -opengl desktop -prefix /opt/usr && make  -j8 && make install && rm -rfv $SOURCES/qt5
 rm -rfv /source/qt5
