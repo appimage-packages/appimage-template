742f89354651dbcdeb3884bbb34f4d08394a93f8
Scarlett Clark <sgclark@kde.org>
2017-05-11 03:32:02 -0700

    Use qt59

--- libs/builddocker.rb
+++ libs/builddocker.rb
@@ -56,7 +56,7 @@ class CI
     workspace = system('pwd')
     p workspace
     @c = Docker::Container.create(
-      'Image' => 'sgclark/trusty-qt580',
+      'Image' => 'sgclark/trusty-qt59',
       'Cmd' => @cmd,
       'Volumes' => {
         '/in' => {},
